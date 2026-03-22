d97564cf3e6b09afc0f4afd91fa29e3dae624cd9
AlanIsaacToroHolguin <alan.software.arch@gmail.com>
2026-03-22 18:58:57 -0500

    string type

--- 02_dataypes/chapter_6.py
+++ 02_dataypes/chapter_6.py
@@ -1,37 +1,57 @@
-# String in pyhton: cabe resaltar que es siempre mutable no puede ser  inmutable ya que no cambia asi que en la memoria siempre se crea una nueva refereencia
+# -----------------------------------
+#  Strings in Python
+# -----------------------------------
+# In Python, strings are IMMUTABLE.
+# This means once a string is created, it cannot be changed.
+# Any modification creates a new object in memory.
 
-# todo lo que este dentro de comillas dobles  es basciamwente un string
+# Everything inside quotes (" " or ' ') is considered a string.
 
-# entonces sobre string veremos  lo que es core,indexing y slicing
-
+# -----------------------------------
+# ☕ Example Variables
+# -----------------------------------
 chai_type = "Ginger chai"
-
 customer_name = "Alan Toro"
 
 print(f"Customer: {customer_name}, Order: {chai_type}")
 
+# -----------------------------------
+#  Indexing & Slicing
+# -----------------------------------
+# Each character in a string has an index starting from 0.
+# The ending index in slicing is NOT inclusive.
+
 chai_description = "Aromatic and bold"
 
-#indexing :significa que cada letra esta representada por un numero empezando por cero and  the last number is not inclusive.
-print(f"First word: {chai_description[0:0]}")  # si lo dejamos asi no imprime nad apor eso para imprimir por completo aromatic deben ser con el numero 8 
+#  Empty slice (start and end are the same)
+print(f"Empty slice: '{chai_description[0:0]}'")
 
-print(f"First word: {chai_description[0:8]}")  # imprime aromatic
+#  Extracting "Aromatic"
+print(f"First word: {chai_description[0:8]}")
 
-print(f"Every two letters: {chai_description[0:8:2]}")  # imprime armtc and bold
+#  Taking every 2 characters
+print(f"Every two letters: {chai_description[0:8:2]}")
 
-print(f"Last word: {chai_description[12:]}")  # imprime bold
+#  Extracting "bold"
+print(f"Last word: {chai_description[12:]}")
 
-print(f"Reverse: {chai_description[::-1]}")  # imprime bold and aromatic al reves
+#  Reversing the string
+print(f"Reversed: {chai_description[::-1]}")
 
+# -----------------------------------
+# 🌍 Encoding & Decoding
+# -----------------------------------
+# When working with special characters (like accents),
+# it's a good practice to encode strings using UTF-8.
 
-#cuando hay caracteres espciales y simbolos o para otros idiomas es buena pracctica usar un ecoded:lale
 label_text = "Chai Spécial"
 
+# Encode string into bytes
 encoded_label = label_text.encode("utf-8")
 
-print(f"Non encoded label: {label_text}")
+print(f"Original label: {label_text}")
 print(f"Encoded label: {encoded_label}")
 
-# is importatn create a decoded label too
+# Decode bytes back to string
 decoded_label = encoded_label.decode("utf-8")
 print(f"Decoded label: {decoded_label}")
